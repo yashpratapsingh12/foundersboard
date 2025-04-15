79fc44d6abb2752df99638cd6d1cd0fc5897ca37
yashpratapsingh12 <yashpratapsingh577@gmail.com>
2025-04-15 20:04:35 +0530

    starting up

--- app/page.tsx
+++ app/page.tsx
@@ -95,7 +95,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          Go toxtjs.org →
         </a>
       </footer>
     </div>
