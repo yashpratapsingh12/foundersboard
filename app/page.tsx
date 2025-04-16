d1f1e403c5388c6fc2c44d9d1c05c758741e79a9
yashpratapsingh12 <yashpratapsingh577@gmail.com>
2025-04-16 21:18:00 +0530

    typos

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="flex justify-center items-center h-screen">FoursHub</h1>
+    <h1 className="flex justify-center items-center h-screen">Foundershub</h1>
   );
 }
