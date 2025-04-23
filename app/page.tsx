ac24e5d1fcb5bad0aa4e1c7f46db0db9683adec3
yashpratapsingh12 <yashpratapsingh577@gmail.com>
2025-04-23 23:29:38 +0530

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <h1 className="flex justify-center items-center h-screen">Foundershub</h1>
-  );
+  return <h1 className="flex justify-center items-center">Foundershub</h1>;
 }
