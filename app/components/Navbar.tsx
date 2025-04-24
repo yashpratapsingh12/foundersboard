73211e43b549fb18af48448fed63fc22c71d3133
yashpratapsingh12 <yashpratapsingh577@gmail.com>
2025-04-24 23:07:24 +0530

    nav comp

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import logo from "../../public/logo.png";
 
 const Navbar = () => {
-  return <div>NAvbar</div>;
+  return (
+    <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+      <nav>
+        <Link href="/">
+          <Image src={logo} alt="logo" width={144} height={30} />
+        </Link>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
