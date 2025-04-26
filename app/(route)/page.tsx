f8bcc818417602da7f8eb91191a93aec9027d5fa
yashpratapsingh12 <yashpratapsingh577@gmail.com>
2025-04-27 01:12:44 +0530

    searchComp

--- app/(route)/page.tsx
+++ app/(route)/page.tsx
@@ -1,13 +1,19 @@
 import Image from "next/image";
+import SearchForm from "../components/SearchForm";
 
 export default function Home() {
   return (
     <>
-      <section className="w-full bg-[#EE2B69] min-h-[530px] flex justify-center items-center flex-col py-10 px-6"></section>
-      <h1 className="uppercase bg-black px-6 py-2 font-extrabold  text-white text-center my-2 max-w-5xl mx-auto">
-        Pitch Your Startup
-        <br /> Connect with Entrepreneur
-      </h1>
+      <section className="w-full bg-[#EE2B69] min-h-[530px] flex justify-center items-center flex-col py-10 px-6  ">
+        <h1 className="uppercase bg-black px-6 py-10 font-extrabold  text-white text-3xl md:text-3xl text-center my-2 w-full mx-auto ">
+          Pitch Your Startup
+          <br /> Connect with Entrepreneur
+        </h1>
+        <p className="font-medium text-white text-[20px] text-center">
+          Submit Ideas , Vote on Pitches ,and Get Noticed in Virtual
+        </p>
+        <SearchForm />
+      </section>
     </>
   );
 }
---
+++ app/components/SearchForm.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Form from "next/form";
+
+const SearchForm = () => {
+  return (
+    <Form
+      action="/"
+      scroll={false}
+      className=" w-80 text-center bg-white uppercase font-bold rounded-2xl mt-5 py-3 border-3 border-black"
+    >
+      <input />
+    </Form>
+  );
+};
+
+export default SearchForm;
