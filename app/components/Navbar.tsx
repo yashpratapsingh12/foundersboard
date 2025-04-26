c4f2ffb02aba2e2aa5d37c2071139f6883141770
yashpratapsingh12 <yashpratapsingh577@gmail.com>
2025-04-26 19:24:09 +0530

    added nextauth

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,14 +2,48 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import logo from "../../public/logo.png";
+import { auth, signIn, signOut } from "@/auth";
+import { redirect } from "next/dist/server/api-utils";
+const Navbar = async () => {
+  const session = await auth();
 
-const Navbar = () => {
   return (
     <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
-      <nav>
+      <nav className="flex justify-between items-center">
         <Link href="/">
           <Image src={logo} alt="logo" width={144} height={30} />
         </Link>
+        <div className=" flex items-center gap-5 text-black">
+          {session && session?.user ? (
+            <>
+              <Link href="/startup/create">
+                <span>create</span>
+              </Link>
+
+              <form
+                action={async () => {
+                  "use server";
+                  await signOut({ redirectTo: "/" });
+                }}
+              >
+                <button>SignOut</button>
+              </form>
+
+              <Link href={`/user/${session?.id}`}>
+                <span>{session?.user?.name}</span>
+              </Link>
+            </>
+          ) : (
+            <form
+              action={async () => {
+                "use server";
+                await signIn("github");
+              }}
+            >
+              <button type="submit">Login</button>
+            </form>
+          )}
+        </div>
       </nav>
     </header>
   );
