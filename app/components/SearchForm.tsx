441d015efeb8f07895627dc7a0465dbfe645934a
yashpratapsingh12 <yashpratapsingh577@gmail.com>
2025-04-27 20:08:28 +0530

    sf comp

--- app/components/SearchForm.tsx
+++ app/components/SearchForm.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import Form from "next/form";
 
 const SearchForm = () => {
+  const query = "test";
+  const reset = () => {};
   return (
     <Form
       action="/"
       scroll={false}
       className=" w-80 text-center bg-white uppercase font-bold rounded-2xl mt-5 py-3 border-3 border-black"
     >
-      <input />
+      <input name="query" defaultValue={query} placeholder="Search Starups" />
+      <div>{query && <button type="reset" onClick={reset}></button>}</div>
     </Form>
   );
 };
