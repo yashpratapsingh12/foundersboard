53a8267ae20cdbacbce50c43e03f55274134781e
yashpratapsingh12 <yashpratapsingh577@gmail.com>
2025-05-02 23:38:54 +0530

    flushed dns

--- app/(route)/startup/[id]/page.tsx
+++ app/(route)/startup/[id]/page.tsx
@@ -4,6 +4,8 @@ import { client } from "@/sanity/lib/client";
 import { STARTUP_BY_ID_QUERY } from "@/sanity/lib/queries";
 import { notFound } from "next/navigation";
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 export const experimental_ppr = true;
 
@@ -18,14 +20,27 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
         <p className=" w-45  mx-auto  bg-[#FBE843] px-6 py-3 font-bold rounded-sm uppercase relative before:content-[''] before:absolute before:top-2 before:left-2 before:border-t-[10px] before:border-t-black before:border-r-[10px] before:border-r-transparent after:content-[''] after:absolute after:bottom-2 after:right-2 after:border-b-[10px] after:border-b-black after:border-l-[10px] after:border-l-transparent">
           {formatdate(post?._createdAt)}
         </p>
-        <h1 className="w-full bg-black text-white w-80 mx-auto text-center rounded-sm px-3 py-3 mt-2 uppercase font-bold">
+        <h1 className="w-full bg-black text-white w-80 mx-auto text-center rounded-sm px-3 py-5 mt-2 uppercase font-bold">
           {post.title}
         </h1>
         <p className="text-white text-center mt-2 font-semibold ">
           {post.description}
         </p>
       </section>
-      <div className="text-3xl">{post.title}</div>
+      <section className="px-6 py-10">
+        <img
+          src={post.image}
+          alt="thumbnail"
+          className=" mx-auto  w-auto h-auto rounded-xl"
+        />
+        <div>
+          <div>
+            <Link href={``}>
+              <Image src={post.author.image} alt="user" />
+            </Link>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
