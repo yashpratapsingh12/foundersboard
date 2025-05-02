59c27e67a8b6dc322d9031b2e412d6f479c037f9
yashpratapsingh12 <yashpratapsingh577@gmail.com>
2025-05-02 20:58:16 +0530

    changes

--- app/(route)/startup/[id]/page.tsx
+++ app/(route)/startup/[id]/page.tsx
@@ -9,14 +9,21 @@ export const experimental_ppr = true;
 
 const page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
+
   const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
   if (!post) return notFound();
   return (
     <>
-      <section className="w-full bg-[#EE2B69] !min-h-[230px]">
-        <p className="bg-[#FBE843]">{formatdate(post?._createdAt)}</p>
-        <h1>{post.title}</h1>
-        <p>{post.description}</p>
+      <section className="w-full bg-[#EE2B69] !min-h-[230px] flex justify-center items-center flex-col py-10 px-6 ">
+        <p className=" w-45  mx-auto  bg-[#FBE843] px-6 py-3 font-bold rounded-sm uppercase relative before:content-[''] before:absolute before:top-2 before:left-2 before:border-t-[10px] before:border-t-black before:border-r-[10px] before:border-r-transparent after:content-[''] after:absolute after:bottom-2 after:right-2 after:border-b-[10px] after:border-b-black after:border-l-[10px] after:border-l-transparent">
+          {formatdate(post?._createdAt)}
+        </p>
+        <h1 className="w-full bg-black text-white w-80 mx-auto text-center rounded-sm px-3 py-3 mt-2 uppercase font-bold">
+          {post.title}
+        </h1>
+        <p className="text-white text-center mt-2 font-semibold ">
+          {post.description}
+        </p>
       </section>
       <div className="text-3xl">{post.title}</div>
     </>
