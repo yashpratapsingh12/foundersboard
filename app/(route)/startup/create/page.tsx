64053bad52c5507f3ba64de8a181e17fa058d26a
yashpratapsingh12 <yashpratapsingh577@gmail.com>
2025-05-05 01:13:23 +0530

    added startupForm

--- app/(route)/startup/create/page.tsx
+++ app/(route)/startup/create/page.tsx
@@ -1,7 +1,17 @@
+import StartupForm from "@/components/StartupForm";
 import React from "react";
 
 const page = () => {
-  return <div>Create</div>;
+  return (
+    <>
+      <section className="w-full bg-[#EE2B69] !min-h-[230px]">
+        <h1 className="uppercase bg-black  py-3 font-extrabold  text-white text-3xl md:text-3xl text-center my-2 w-full mx-auto  ">
+          Submit Your Startups
+        </h1>
+      </section>
+      <StartupForm />
+    </>
+  );
 };
 
 export default page;
