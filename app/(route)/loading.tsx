d50df977b7592d63b3906b281f06bc0d7c6b2f35
yashpratapsingh12 <yashpratapsingh577@gmail.com>
2025-05-15 21:45:47 +0530

    bugs

--- app/(route)/loading.tsx
+++ app/(route)/loading.tsx
@@ -2,11 +2,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 import React from "react";
 
 const loading = () => {
-  return (
-    <div>
-      <Skeleton className="w-[100px] h-[20px] rounded-full" />
-    </div>
-  );
+  return <div>Loding</div>;
 };
 
 export default loading;
