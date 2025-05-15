5bc53057d60c0abd561a4cf9f5a98d9b32499c2c
yashpratapsingh12 <yashpratapsingh577@gmail.com>
2025-05-15 18:01:34 +0530

    delete op done

--- components/DeletePost.tsx
+++ components/DeletePost.tsx
@@ -1,9 +1,20 @@
 "use client";
 
 import React from "react";
+import { Button } from "./ui/button";
+import { client } from "@/sanity/lib/client";
+import { toast } from "sonner";
 
 const DeletePost = ({ postid }: { postid: string }) => {
-  return <div>Delete</div>;
+  const handleClick = async () => {
+    await client.delete(postid);
+    toast.success("Startup has been deleted");
+  };
+  return (
+    <>
+      <Button onClick={handleClick}>Delete</Button>
+    </>
+  );
 };
 
 export default DeletePost;
