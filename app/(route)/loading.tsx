2dd42a8094c3a9e5e1f736fc6f054abbb88711a4
yashpratapsingh12 <yashpratapsingh577@gmail.com>
2025-05-15 22:32:15 +0530

    custom loader

--- app/(route)/loading.tsx
+++ app/(route)/loading.tsx
@@ -2,7 +2,11 @@ import { Skeleton } from "@/components/ui/skeleton";
 import React from "react";
 
 const loading = () => {
-  return <div>Loding</div>;
+  return (
+    <div className="flex justify-center items-center h-20">
+      <div className="w-10 h-10 border-4 border-blue-500 border-t-transparent rounded-full animate-spin" />
+    </div>
+  );
 };
 
 export default loading;
